ba98ae6c18672d00a6ba758791e2d2d18a335e66
derrosilee <derrossilee@gmail.com>
2023-04-10 20:50:11 +0300

    Created Blog Model

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from datetime import date
 
 
 # Create your models here.
@@ -27,3 +28,27 @@ class BlogAuthor(models.Model):
         String for representing the model object
         """
         return self.user.username
+
+
+class Blog(models.Model):
+    name = models.CharField(max_length=200)
+    author = models.ForeignKey(BlogAuthor, on_delete=models.SET_NULL, null=True)
+    # Foreign Key used because Blog can only have one author/User, but bloggers can have multiple blog posts.
+    description = models.TextField(max_length=2000, help_text="Enter Your Blog Text Here")
+    post_date = models.DateField(default=date.today)
+
+    class Meta:
+        ordering = ['-post_date']
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular blog instance.:
+        """
+        return reverse('blog-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return self.name
+
