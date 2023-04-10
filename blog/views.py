ccf07e2061e324921ab965868e9ad189f089d093
derrosilee <derrossilee@gmail.com>
2023-04-10 21:07:56 +0300

    added blog class view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Blog, BlogAuthor, BlogComment
+from django.contrib.auth.models import User
+from django.views import generic
 
 
 # Create your views here.
 # TODO:: Change to class list view for all the blogs
-def index(request):
-    return render(request, template_name='blog/index.html')
+class BlogListView(generic.ListView):
+    """
+    Generic class-based view for a list of all blogs.
+    """
+    model = Blog
+    template_name = 'blog/index.html'
