208fee052477aaf5244d9a16f735d89530faa89e
derrosilee <derrossilee@gmail.com>
2023-04-10 20:23:36 +0300

    added index view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+# TODO:: Change to class list view for all the blogs
+def index(request):
+    return render(request, template_name='blog/index.html')
