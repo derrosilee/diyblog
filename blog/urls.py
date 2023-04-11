66c0683f1fc44f7d372db2cac4a0377051900023
derrosilee <derrossilee@gmail.com>
2023-04-11 09:34:55 +0300

    Added Blog-Detail Page

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('blogs/', views.BlogListView.as_view(), name='blogs')
+    path('blogs/', views.BlogListView.as_view(), name='blogs'),
+    path('blog/<int:pk>/', views.BlogDetailView.as_view(), name='blog-detail')
 ]
