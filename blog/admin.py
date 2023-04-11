b2895bbaf90f0c1604cdd851409c8b91d49b1952
derrosilee <derrossilee@gmail.com>
2023-04-11 09:25:32 +0300

    added Blog Category

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Blog, BlogAuthor
+from .models import Blog, BlogAuthor, BlogCategory
 # Register your models here.
 
 
 admin.site.register(Blog)
 
 admin.site.register(BlogAuthor)
+
+admin.site.register(BlogCategory)
