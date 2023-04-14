dfc0acc0b0bd54a5ccfff058fd77a12322c8fa00
derrosilee <derrossilee@gmail.com>
2023-04-14 16:50:52 +0300

    Updated Login Redirect

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/blog/blogs/'
 
 
 # Default primary key field type
