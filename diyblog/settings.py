ec8baba97b8e7943feb666a715e19148781b3067
derrosilee <derrossilee@gmail.com>
2023-04-14 16:42:36 +0300

    added Login Redirect

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -115,6 +115,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
