95231e9d7c6066f9ec4bb7193f18fe996428ca96
derrosilee <derrossilee@gmail.com>
2023-04-14 13:38:59 +0300

    refactored Blog List View

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog, BlogAuthor, BlogComment
 from django.contrib.auth.models import User
 from django.views import generic
@@ -23,9 +23,36 @@ class BlogDetailView(generic.DetailView):
     template_name = 'blog/blog-detail.html'
 
 
-class BlogAuthorListView(generic.ListView):
+class BlogListView(generic.ListView):
     """
     Generic Class Based View for a list view of all Authors
     """
     model = BlogAuthor
     template_name = 'blog/authors.html'
+
+
+class BlogListByAuthorView(generic.ListView):
+    """
+    Generic class-based view for a list of blogs posted by a particular BlogAuthor.
+    """
+    model = Blog
+    # TODO: Add Pagination
+    template_name = 'blog/blog_list_by_author.html'
+
+    def get_queryset(self):
+        """
+        Return list of Blog objects created by BlogAuthor (author id specified in URL)
+        """
+        id = self.kwargs['pk']
+        target_author = get_object_or_404(BlogAuthor, pk=id)
+        return Blog.objects.filter(author=target_author)
+
+    def get_context_data(self, **kwargs):
+        """
+         Add BlogAuthor to context so they can be displayed in the template
+        """
+        # Call the base implementation first to get a context
+        context = super(BlogListByAuthorView, self).get_context_data(**kwargs)
+        # Get the blogger object from the "pk" URL parameter and add it to the context
+        context['blogger'] = get_object_or_404(BlogAuthor, pk=self.kwargs['pk'])
+        return context
