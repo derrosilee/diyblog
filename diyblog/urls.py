f0c9c838aa2da4592f2cd2ee559d67bd6f06e24e
derrosilee <derrossilee@gmail.com>
2023-04-14 14:28:51 +0300

    Added auth url

--- diyblog/urls.py
+++ diyblog/urls.py
@@ -11,5 +11,12 @@ urlpatterns += [
     path('blog/', include('blog.urls'))
 ]
 
+# Add Django site authentication urls (for login, logout, password management)
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+
+
 if settings.DEBUG:  # new
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
