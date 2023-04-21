1b17041c546125e206e243733be4829bd55aabd3
derrosilee <derrossilee@gmail.com>
2023-04-21 09:22:07 +0300

    Added Reg Logic

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
 
 
@@ -7,3 +9,15 @@ def sign_up(request):
     if request.method == 'GET':
         form = RegisterForm
         return render(request, 'registration/register.html', {'form': form})
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'You have singed up successfully.')
+            login(request, user)
+            return redirect('blogs')
+        else:
+            return render(request, 'registration/register.html', {'form': form})
