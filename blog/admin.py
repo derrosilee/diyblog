a28704072e7d1cb8d6e5b23978060097b05dbbc6
derrosilee <derrossilee@gmail.com>
2023-04-21 09:35:23 +0300

    Added Blog Comment to admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,27 @@
 from django.contrib import admin
-from .models import Blog, BlogAuthor, BlogCategory
-# Register your models here.
+from .models import BlogAuthor, Blog, BlogComment
 
-
-admin.site.register(Blog)
-
+# Minimal registration of Models.
 admin.site.register(BlogAuthor)
+admin.site.register(BlogComment)
 
-admin.site.register(BlogCategory)
+
+class BlogCommentsInline(admin.TabularInline):
+    """
+    Used to show 'existing' blog comments inline below associated blogs
+    """
+    model = BlogComment
+    max_num = 0
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    """
+    Administration object for Blog models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - orders fields in detail view (fields), grouping the date fields horizontally
+     - adds inline addition of blog comments in blog view (inlines)
+    """
+    list_display = ('name', 'author', 'post_date')
+    inlines = [BlogCommentsInline]
