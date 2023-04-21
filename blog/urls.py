0f819ac939336eacd5c31dceec9155a565766cf5
derrosilee <derrossilee@gmail.com>
2023-04-21 09:33:19 +0300

    Added Blog Comment url

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,16 @@
 from . import views
 from django.urls import path
 
-
 urlpatterns = [
     path('blogs/', views.BlogListView.as_view(), name='blogs'),
     path('blog/<int:pk>/', views.BlogDetailView.as_view(), name='blog-detail')
 ]
 
-urlpatterns +=[
+urlpatterns += [
     path('bloggers/', views.BlogAuthorListView.as_view(), name='bloggers'),
     path('blogger/<int:pk>/', views.BlogListByAuthorView.as_view(), name='blogs-by-author')
+]
+
+urlpatterns += [
+    path('blog/<int:pk>/comment/', views.BlogCommentCreate.as_view(), name='blog_comment'),
 ]
